6a7601cb0c13aa94a6fc506204c559af3a1fe959
damiencorpataux <d@mien.ch>
2012-06-18 12:04:39 +0200

    API entry point to return jsonified exceptions + message

--- service.php
+++ service.php
@@ -6,5 +6,13 @@ $params = array_merge(
     //array('provider' => 'fuzzy'),
     $_REQUEST
 );
-$a = new TlApi($params);
-print $a->get();
+
+try {
+    $a = new TlApi($params);
+    print $a->get();
+} catch (Exception $e) {
+    print json_encode(array(
+        'error' => true,
+        'message' => $e->getMessage()
+    ));
+}
